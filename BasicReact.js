2567241648ebf26008a88c5df050b38a73a1b4b6
Aditya-Chaudary05 <140259231+JanwinAditya@users.noreply.github.com>
2025-01-14 23:06:02 +0530

    Add files via upload

---
+++ BasicReact.js
@@ -0,0 +1,11 @@
+
+import { createRoot } from "react-dom/client"
+
+const root = createRoot(document.getElementById("root"))
+
+root.render(
+    <list><h1>Why to learn React?</h1>
+    <li>Its a top front-end library </li>
+    <li>It is one of the framework ,I will be using the most</li>
+    <li>It will deepen my understanding on React</li>
+    </list>)
